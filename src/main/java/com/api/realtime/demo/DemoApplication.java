90fdc4b7c967033b185f2a30c8420b0bddc3055b
LuneRemer <europejumin@gmail.com>
2021-09-20 17:31:46 +0900

    war deployment preparation

--- src/main/java/com/api/realtime/demo/DemoApplication.java
+++ src/main/java/com/api/realtime/demo/DemoApplication.java
@@ -3,6 +3,8 @@ package com.api.realtime.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,7 +12,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 //@ComponentScan("com.api.realtime.demo.domain")
 //@EntityScan(basePackages = {"com.api.realtime.demo.entity"})
 //@EnableJpaRepositories(basePackages = {"com.api.realtime.demo.repository"})
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(DemoApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
